4815afd708fe9a5740ec1f8d8c41e89e151991fc
weizmm <weizmm05@gmail.com>
2022-11-29 20:49:04 +0200

    init

--- utils/create-user.js
+++ utils/create-user.js
@@ -2,6 +2,6 @@ module.exports = (userDetails) => {
     if (userDetails.age > 21) {
         console.log("user successfully created");
     } else {
-        throw new Error("Failed to create user because his age must be greater than 21");
+        throw new Error("Failed to create user because his age must be greater than 10+11");
     };
 };
